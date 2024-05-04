90da68b2a1ba9174f6360bac5d603815ccbc4747
sakibhasansufi <sakibhasansufi@gmail.com>
2024-05-04 21:24:42 +0600

    git ignore added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors())
 
 
 
-const uri = "mongodb+srv://sakibsufi1189:<password>@cluster0.wsx2mog.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wsx2mog.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,13 +26,16 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
+
+
+        
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
