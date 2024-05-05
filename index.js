604f6c98a5a72c4828ed20f34b4fb06b69ce4882
sakibhasansufi <sakibhasansufi@gmail.com>
2024-05-05 19:58:14 +0600

    delete functionality applied

--- index.js
+++ index.js
@@ -72,6 +72,14 @@ async function run() {
             res.send(result)
         })
 
+
+        app.delete('/bookings/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const result = await bookingsCollection.deleteOne(query);
+            res.send(result);
+        })
+
         
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
